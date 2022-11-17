d30e724d19fc8d1f824c817bb339e88b7c745521
LaisBS <96962027+LaisBS@users.noreply.github.com>
2022-11-17 14:57:02 -0300

    Feat: healthy body

--- src/utils/reposData.ts
+++ src/utils/reposData.ts
@@ -27,4 +27,8 @@ export const RepoImgs = [
     id: 492970913,
     img: "https://res.cloudinary.com/dqqw3fvn2/image/upload/v1667019335/Captura_de_tela_2022-10-29_015447_gajhlt.jpg",
   },
+  {
+    id: 567397785,
+    img: "https://res.cloudinary.com/dqqw3fvn2/image/upload/v1668707606/GymAcademiaFitnessMusuca%C3%A7%C3%A3o_qqzgib.jpg",
+  },
 ];
